f9648354c0cf09de1bdb086ffcf741c10aa66dcd
depthfirst <donggongfu@gmail.com>
2022-06-20 00:25:47 +0800

    style: UIWindow+NEExtension.m

--- NetworkEye/NetworkEye/UIWindow+NEExtension.m
+++ NetworkEye/NetworkEye/UIWindow+NEExtension.m
@@ -11,31 +11,27 @@
 
 @implementation UIWindow (NEExtension)
 
-#if defined(DEBUG)||defined(_DEBUG)
+#if defined(DEBUG) || defined(_DEBUG)
 - (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
-    
     if (event.type == UIEventTypeMotion && event.subtype == UIEventSubtypeMotionShake) {
         [[NEShakeGestureManager defaultManager] showAlertView];
     }
-    
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-    
-    int twoFingerTag=NO;
-    if ([event allTouches].count==2) {
-        for (UITouch *touch in [event allTouches]) {
-            if (touch.tapCount==1) {
-                twoFingerTag=YES;
-            }else{
-                twoFingerTag=NO;
+    int twoFingerTag = NO;
+    if (event.allTouches.count == 2) {
+        for (UITouch *touch in event.allTouches) {
+            if (touch.tapCount == 1) {
+                twoFingerTag = YES;
+            } else {
+                twoFingerTag = NO;
             }
         }
     }
     if (twoFingerTag) {
         [[NEShakeGestureManager defaultManager] showAlertView];
     }
-    
 }
 #endif
 
